cd6cdaf4f427c47998271a68e60f4543f65622e8
gwdixon <93284217+gwdixon@users.noreply.github.com>
2022-02-22 17:46:39 -0500

    Delete progress-knight-community directory

--- progress-knight-community/js/data/item.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-    //"Steel longsword": {category: "Misc", expense:     1000, effect: {type: "category_xp",  value: 2  , category: "Military"}, 
-    //                    tooltip: "A fine blade used to slay enemies even quicker in combat and therefore gain more experience.", },
-    //"Sapphire charm":  {category: "Misc", expense:    50000, effect: {type: "category_xp",  value: 1.5, category: "Arcane" }, 
-    //                    tooltip: "Embedded with a rare sapphire, this charm activates more mana channels within your body, providing a much easier time learning magic.", },
-}
